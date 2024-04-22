9fd23a99ef83decd5c07561cd1fbe538ee7cfdfd
yukitakeshita <yuki.takeshita96@gmail.com>
2024-04-22 11:08:08 +0900

    Lesson_ECsite

---
+++ src/main/java/jp/co/internous/ecsite/model/domain/MstGoods.java
@@ -0,0 +1,47 @@
+package jp.co.internous.ecsite.model.domain;
+
+import java.sql.Timestamp;
+
+public class MstGoods {
+	
+	private int id;
+	private String goodsName;
+	private int price;
+	private Timestamp createAt;
+	private Timestamp updatedAt;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public Timestamp getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(Timestamp createAt) {
+		this.createAt = createAt;
+	}
+	public Timestamp getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Timestamp updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	
+
+}
